fced1a26d752af586a916a9b382c0aef04599bca
Dymytrian <moyaelektronka@ukr.net>
2021-12-27 14:13:35 +0200

    adding changes to file test_1funcpytest.py

---
+++ homeworks/testuvannya/testingmy/test_1funcpytest.py
@@ -0,0 +1,20 @@
+import pytest
+
+import function1_forpytest as fp
+
+def test_division():
+    assert fp.division(3,5) == 0.6
+    assert fp.division(50,2) == 25
+    assert fp.division(5,10) ==0.5
+
+
+
+
+
+
+
+
+
+
+
+
