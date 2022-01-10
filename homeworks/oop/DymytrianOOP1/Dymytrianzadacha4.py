ba71494d05ef3f3db48366edd1101a3097fa35e2
Dymytrian <moyaelektronka@ukr.net>
2022-01-10 22:37:07 +0200

    Adding changes to file Dymytrianzadacha4.py

--- homeworks/oop/DymytrianOOP1/Dymytrianzadacha4.py
+++ homeworks/oop/DymytrianOOP1/Dymytrianzadacha4.py
@@ -2,26 +2,27 @@
 
 class Vehicle:
 
-    def __init__(self, maxspeed, mileage, capasyty):
-        self.maxspeed=maxspeed
-        self.mileage=mileage
-        self.capasyty=capasyty
+    def __init__(self, maxspeed, mileage, capacity):
+        self.maxspeed = maxspeed
+        self.mileage = mileage
+        self.capacity = capacity
 
 
 #2Create a child class Bus that will inherit all the variables and methods of the Vehicle class and will have seating_capacity own method
 
-class Buss(Vehicle):
+class Bus(Vehicle):
 
-    def seat_capacity (self):
-        print (f'capasyty: {self.capasyty}')
+    def seating_capacity (self):
+        print (f'seating_capasyty: {self.capacity}')
 #3Determine which class a given Bus object belongs to (Check type of an object)
 
-School_Buss=Buss(160,200000,200)
-print(type(School_Buss))
+School_Bus = Bus(160, 200000, 100)
+print(type(School_Bus))
+School_Bus.seating_capacity()
 
 #4Determine if School_bus is also an instance of the Vehicle class
 
-print(isinstance(School_Buss,Vehicle))
+print(isinstance(School_Bus, Vehicle))
 
 #5Create a new class School with get_school_id and number_of_students instance attributes
 
@@ -30,20 +31,24 @@ class School:
         self.school_id = school_id
         self.number_of_students = number_of_students
 
-    def print_number_of_students(self):
-        print('We a have next', {self.number_of_students})
-    def get_school_id(self):
-        print("Id number school ", {self.school_id})
 
+    def get_school_id(self):
+        print("Id number school ", self.school_id)
+    def get_number_of_students(self):
+        print('We a have next', self.number_of_students)
+
+My_School = School(2, 1500)
+My_School.get_school_id()
+My_School.get_number_of_students()
 #7Polymorphism: Create two classes: Bear, Wolf. Both of them should have make_sound method. Create two instances, one of Bear and one of Wolf,
 #make a tuple of it and by using for call their action using the same method.
 
 class Volf:
 
-    def __init__(self,sound):
-        self.sound=sound
+    def __init__(self, sound):
+        self.sound = sound
     def make_sound (self):
-        print("Volf sound:",self.sound)
+        print("Volf sound:", self.sound)
 
 
 Volf1=Volf("UUUUU")
@@ -51,19 +56,19 @@ Volf1.make_sound()
 
 class Bear:
 
-    def __init__(self,sound):
-        self.sound=sound
+    def __init__(self, sound):
+        self.sound = sound
     def make_sound (self):
         print("Bear sound:",self.sound)
 
 
-Bear1=Bear("EEEEE")
+Bear1 = Bear("EEEEE")
 Bear1.make_sound()
 
-predators=tuple([Volf1,Bear1])
+predators = tuple([Volf1,Bear1])
 
-for predators in predators:
-    print( predators.make_sound())
+for predator in predators:
+    print(predator.make_sound())
 
 
 
